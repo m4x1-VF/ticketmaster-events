ac7bbe9343e834735e079c8b42f8167651a00523
Maximiliano Vilariño <maxi.vilarino@gmail.com>
2023-08-23 01:15:45 +0200

    Implementación de Like y UnLike en locaStorage

--- src/components/Events/components/Eventitem/index.jsx
+++ src/components/Events/components/Eventitem/index.jsx
@@ -1,12 +1,28 @@
+import HearthFilled from "../../../../assets/hearth-filled.png";
+import HearthUnfilled from "../../../../assets/hearth-unfilled.png";
+import useLikeEvents from "../../../../hooks/useLikeEvents";
+
 const EventItem = ({ id, date, time, image, name, onEventClick }) => {
+  const { isEventLiked, toggleEventLike } = useLikeEvents(id);
+
   const handleSeeMoreClick = (event) => {
     event.stopPropagation();
     onEventClick(id);
   };
 
+  const handleHearthClick = () => {
+    toggleEventLike();
+  };
+
   return (
     <div className="flex my-[24px] items-start]">
-      <figure className="ml-[14px]">
+      <figure className="ml-[14px] relative">
+        <img
+          src={isEventLiked ? HearthFilled : HearthUnfilled}
+          alt="Hearth button"
+          className="absolute w-6 top-[10px] left-[10px] z-10 cursor-pointer"
+          onClick={handleHearthClick}
+        />
         <img src={image} alt={name} width={200} height={200} />
       </figure>
       <section className="flex flex-col items-start ml-[14px]">
